59aa18b0427113e02b621fcd29c553e13a656d87
tlaranje <tlaranje@student.42.porto.com>
2025-12-16 14:09:09 +0000

    :)

--- Python_Module_03/ex6/ft_analytics_dashboard.py
+++ Python_Module_03/ex6/ft_analytics_dashboard.py
@@ -3,6 +3,8 @@ def list_comprehension(socres):
     print("=== List Comprehension Examples ===")
     high_scores = [num > 2000 for num in socres]
     print(f"{high_scores}")
+
+
 if __name__ == "__main__":
     print("=== Game Analytics Dashboard ===\n")
     s = [("alice", 2000), ("charlie", 3000), ("diana", 4000)]
